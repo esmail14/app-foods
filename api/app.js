c12fcc4b5b80090536b098c0f869905918d937f1
DESKTOP-HMAFFKV\juana <juantonio.ordonez@gmail.com>
2025-12-17 17:41:15 +0100

    fix: mejorar página de landing para pwa

---
+++ api/app.js
@@ -0,0 +1,17 @@
+const { createExpoMiddleware } = require('expo-server');
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, '../public')));
+
+app.use(createExpoMiddleware({
+  projectRoot: __dirname,
+}));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+module.exports = app;
